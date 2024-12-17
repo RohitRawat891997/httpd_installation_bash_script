fadfec422673a3ae79e41e7dea7c36e7ebe2c818
RohitRawat891997 <rohitrawat891997@gmail.com>
2024-12-17 00:55:02 +0000

    second  change

--- httpd.sh
+++ httpd.sh
@@ -2,4 +2,5 @@
 sudo  yum install -y httpd
 sudo  systemctl  start  httpd
 sudo  systemctl  enable  httpd
+sed -i   's/^Listen 80/Listen 8080/g'  /etc/httpd/conf/httpd.conf
 echo "hi"  >  /var/www/html/index.html
